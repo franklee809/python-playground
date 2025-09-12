183aec45cd5b8a967df0bd576d373b6e444c6f4d
Frank <franklee809@gmail.com>
2025-09-12 15:18:22 +0800

    build: change dev image

--- flask-app/docker-run.sh
+++ flask-app/docker-run.sh
@@ -5,5 +5,8 @@ docker build -t flask-app .
 docker run -d \
     --name flask-app \
     -p 5000:5000 \
-    flask-app
+    -w  /app
+    -v "$(pwd):/app" \ 
+    flask-app 
+    sh -c 'flask run'
 
